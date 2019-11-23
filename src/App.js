9491d56323eea5ba5b0b46b4fb95b60fdb1dfe78
ulugbek989898 <u.isroilov@student.inha.uz>
2019-11-23 11:16:56 +0500

    Created project with create react app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Homepage from "./homepage.component";
+
+
+class  App extends React.Component {
+  render() {
+
+    return (
+        <div >
+            <Homepage/>
+        </div>
+    );  }
+
+}
+
+export default App;
