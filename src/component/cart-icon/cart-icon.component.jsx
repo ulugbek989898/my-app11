c4148397ed8d47d507ceda851ec95e6d342b870a
ulugbek989898 <u.isroilov@student.inha.uz>
2019-12-10 13:08:13 +0500

    added CartIcon and CartDropDown component to our application also implemented cart reducer

---
+++ src/component/cart-icon/cart-icon.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {toggleCartHidden} from '../../redux/cart/cart.actions';
+import {connect} from 'react-redux';
+import './cart-icon.styles.scss';
+import {ReactComponent as ShoppinIcon} from  '../../assets/shopping-bag.svg';
+
+const CartIcon = ({toggleCartHidden}) => (
+    <div className="cart-icon" onClick={toggleCartHidden}>
+        <ShoppinIcon className='shopping-icon' />
+        <span className="item-count">0</span>
+    </div>
+);
+
+ const mapDispatchToProps = dispatch => ({
+     toggleCartHidden: () => dispatch(toggleCartHidden())
+ })
+
+export default connect(null,mapDispatchToProps) (CartIcon);
